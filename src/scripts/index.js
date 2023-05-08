417c6ace3e117e4203288321d8f9c5efa4b46a51
unknown <shalamberidzeg56@gmail.com>
2023-05-08 21:12:59 +0400

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,14 @@
+
+ //import '../styles/index.scss'
+
+ const menuList = document.querySelectorAll('#header-menu li')
+
+ for (let i = 0; i < menuList.length; i++) {
+    const li = menuList[i]
+     const target = li.getAttribute('target')
+    li.addEventListener('click',function() {
+         document.getElementById(target).scrollIntoView({behavior:'smooth'});
+    })
+ }
+
+
